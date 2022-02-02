5e1f17a31838c8754d336109e3cd50a9959ed346
frnowe001 <frnowe001@myuct.ac.za>
2022-02-02 19:14:17 +0200

    All Indexes added

--- conversions/src/main/java/com/owenfranke/conversions/controller/IndexController.java
+++ conversions/src/main/java/com/owenfranke/conversions/controller/IndexController.java
@@ -33,6 +33,119 @@ public class IndexController {
     public String fromCm(@PathVariable(value = "input") double input) {
         return conversion.centimetre(input);
     }
-    
+
+    @RequestMapping("length/metre/{input}")
+    public String fromMetre(@PathVariable(value = "input") double input) {
+        return conversion.metre(input);
+    }
+
+    @RequestMapping("length/foot/{input}")
+    public String fromFoot(@PathVariable(value = "input") double input) {
+        return conversion.foot(input);
+    }
+
+    @RequestMapping("length/kilometre/{input}")
+    public String fromKm(@PathVariable(value = "input") double input) {
+        return conversion.kilometre(input);
+    }
+
+    @RequestMapping("length/mile/{input}")
+    public String fromMile(@PathVariable(value = "input") double input) {
+        return conversion.mile(input);
+    }
+
+    // Area:
+
+    @RequestMapping("area/squarefoot/{input}")
+    public String fromSF(@PathVariable(value = "input") double input) {
+        return conversion.squareFoot(input);
+    }
+
+    @RequestMapping("area/rood/{input}")
+    public String fromRood(@PathVariable(value = "input") double input) {
+        return conversion.rood(input);
+    }
+
+    @RequestMapping("area/acre/{input}")
+    public String fromAcre(@PathVariable(value = "input") double input) {
+        return conversion.acre(input);
+    }
+
+    @RequestMapping("area/hectare/{input}")
+    public String fromHectare(@PathVariable(value = "input") double input) {
+        return conversion.hectare(input);
+    }
+
+    @RequestMapping("area/squaremetre/{input}")
+    public String fromSM(@PathVariable(value = "input") double input) {
+        return conversion.squareMetre(input);
+    }
+
+    @RequestMapping("area/sqaureKilometer/{input}")
+    public String fromSquareKilometer(@PathVariable(value = "input") double input) {
+        return conversion.squareKilometre(input);
+    }
+
+    // Mass:
+    @RequestMapping("mass/ounce/{input}")
+    public String fromOunce(@PathVariable(value = "input") double input) {
+        return conversion.ounce(input);
+    }
+
+    @RequestMapping("mass/pound/{input}")
+    public String fromPound(@PathVariable(value = "input") double input) {
+        return conversion.pound(input);
+    }
+
+    @RequestMapping("mass/stone/{input}")
+    public String fromStone(@PathVariable(value = "input") double input) {
+        return conversion.stone(input);
+    }
+
+    @RequestMapping("mass/miligrams/{input}")
+    public String fromMiligrams(@PathVariable(value = "input") double input) {
+        return conversion.miligrams(input);
+    }
+
+    @RequestMapping("mass/grams/{input}")
+    public String fromGrams(@PathVariable(value = "input") double input) {
+        return conversion.grams(input);
+    }
+
+    @RequestMapping("mass/kilograms/{input}")
+    public String fromKM(@PathVariable(value = "input") double input) {
+        return conversion.kiloGrams(input);
+    }
+
+    // Volumes:
+    @RequestMapping("volume/ounce/{input}")
+    public String fromFluidOunce(@PathVariable(value = "input") double input) {
+        return conversion.fluidOunce(input);
+    }
+
+    @RequestMapping("volume/pint/{input}")
+    public String fromPint(@PathVariable(value = "input") double input) {
+        return conversion.pint(input);
+    }
+
+    @RequestMapping("volume/gallon/{input}")
+    public String fromGallon(@PathVariable(value = "input") double input) {
+        return conversion.gallon(input);
+    }
+
+    @RequestMapping("volume/mililitre/{input}")
+    public String fromMililitre(@PathVariable(value = "input") double input) {
+        return conversion.mililitre(input);
+    }
+
+    @RequestMapping("volume/litre/{input}")
+    public String fromLitre(@PathVariable(value = "input") double input) {
+        return conversion.litre(input);
+    }
+
+    @RequestMapping("volume/kilolitre/{input}")
+    public String fromKilolitre(@PathVariable(value = "input") double input) {
+        return conversion.kiloLitre(input);
+    }
 
 }
